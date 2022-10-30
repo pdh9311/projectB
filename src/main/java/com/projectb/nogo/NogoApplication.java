c4cb84fecc1b2a9cb9a8aa136bfaadafdb9e3cbf
pdh9311 <padohy@gmail.com>
2022-10-30 23:19:04 +0900

    init: projectB

---
+++ src/main/java/com/projectb/nogo/NogoApplication.java
@@ -0,0 +1,13 @@
+package com.projectb.nogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NogoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NogoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projectb/nogo/NogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectb.nogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NogoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
