b22b8cd23934c90489e2f777688e955ee9f65a72
Jeon-TaeUng <unge0323@gmail.com>
2022-11-07 22:28:21 +0900

    ADD and MOD database config

---
+++ src/main/java/com/projectb/nogo/config/DataSourceConfig.java
@@ -0,0 +1,32 @@
+package com.projectb.nogo.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+@RequiredArgsConstructor
+public class DataSourceConfig {
+	private final Environment environment;
+
+	private final String DRIVER = "spring.datasource.driver-class-name";
+	private final String URL = "spring.datasource.url";
+	private final String USER = "spring.datasource.username";
+	private final String PASSWORD = "spring.datasource.password";
+
+	@Bean
+	DataSource dataSource() {
+		DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+		driverManagerDataSource.setUrl(environment.getProperty(URL));
+		driverManagerDataSource.setUsername(environment.getProperty(USER));
+		driverManagerDataSource.setPassword(environment.getProperty(PASSWORD));
+		driverManagerDataSource.setDriverClassName(environment.getProperty(DRIVER));
+		return driverManagerDataSource;
+	}
+}
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS employer;
+CREATE TABLE IF NOT EXISTS employer
+(
+    i bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id varchar(100) NOT NULL,
+    pw varchar(100) NOT NULL,
+    email varchar(100),
+    business_number varchar(100) NOT NULL,
+    expiration_period datetime NOT NULL,
+    agree_service boolean NOT NULL,
+    agree_personal_info boolean NOT NULL,
+    agree_sms boolean,
+    agree_email boolean
+);
