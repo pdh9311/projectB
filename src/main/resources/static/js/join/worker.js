f75dba010ecdce766c8e99bbe8bec7a478704fd7
Jeon-TaeUng <unge0323@gmail.com>
2022-11-07 22:20:53 +0900

    ADD JS WORKER JOIN

---
+++ src/main/resources/static/js/join/worker.js
@@ -0,0 +1,96 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const agreeAll = document.getElementById("agree-chk-all");
+    const agreeMembers = document.querySelectorAll(".agree-member");
+    const joinForm = document.getElementById("submit");
+
+    agreeAll.addEventListener('click', function () {
+        agreeMembers.forEach((agreeMember) => {
+            agreeMember.checked = agreeAll.checked;
+        });
+    });
+
+    joinForm.addEventListener('click', function () {
+
+        let submitDate = {};
+
+        const agreeService = document.getElementById("agreeService").checked;
+        const agreePersonalInfo = document.getElementById("agreePersonalInfo").checked;
+        const agreeSms = document.getElementById("agreeSms").checked;
+        const agreeEmail = document.getElementById("agreeEmail").checked;
+        const id = document.getElementById("id").value;
+        const password = document.getElementById("password").value;
+        const rePassword = document.getElementById("rePassword").value;
+        const email = document.getElementById("email").value;
+        const name = document.getElementById("name").value;
+        const phone = document.getElementById("phone").value;
+        const adr = document.getElementById("adr").value;
+        const adrDetail = document.getElementById("adrDetail").value;
+        const historyStartDate = document.querySelectorAll(".history-start-date")[0].value;
+        const historyEndDate = document.querySelectorAll(".history-end-date")[0].value;
+        const historyContent = document.querySelectorAll(".history-content")[0].value;
+        const photoContent = document.querySelectorAll(".photo-content")[0].value;
+        const radioButton = document.querySelectorAll(".radio-button");
+
+        let radioChked = "";
+        radioButton.forEach((radio) => {
+            if(radio.checked){
+                radioChked = radio.value;
+            }
+        });
+
+        let personal = {};
+        personal.agreeService= agreeService;
+        personal.agreePersonalInfo= agreePersonalInfo;
+        personal.agreeSms= agreeSms;
+        personal.agreeEmail= agreeEmail;
+        personal.id= id;
+        personal.password= password;
+        personal.rePassword= rePassword;
+        personal.email= email;
+        personal.name= name;
+        personal.phone= phone;
+        personal.adr= adr;
+        personal.adrDetail= adrDetail;
+        personal.period= radioChked;
+
+        submitDate.personal=personal;
+
+        let photos = {};
+        let photoList = [];
+        photos.photoContent= photoContent;
+        photoList.push(photos);
+        submitDate.photoList=photoList;
+
+        let historys = {};
+        let historyList = [];
+        historys.historyStartDate= historyStartDate;
+        historys.historyEndDate= historyEndDate;
+        historys.historyContent= historyContent;
+        historyList.push(historys)
+        submitDate.historyList=historyList;
+
+        console.log("=submitDate=")
+        console.log("submitDate" , submitDate)
+        var httpRequest  = new XMLHttpRequest();
+
+        httpRequest.open('POST', '/join/worker',true);
+        httpRequest.responseType = "json";
+        httpRequest.setRequestHeader('Content-Type', 'application/json');
+        httpRequest.send(JSON.stringify(submitDate));
+
+        httpRequest.onload = () => {
+            //통신 성공
+            if (httpRequest.status == 200) {
+                console.log(httpRequest.response);
+                console.log("통신 성공");
+            } else {
+                //통신 실패
+                console.log("통신 실패");
+            }
+        }
+    });
+
+
+
+});
